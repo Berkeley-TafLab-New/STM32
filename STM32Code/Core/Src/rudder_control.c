193750222388300cc381c87d2a9b8d010cd3e033
Miiu64 <vikram@nandi.net>
2025-04-28 21:56:44 -0700

    changed rudder_move_to

--- STM32Code/Core/Src/rudder_control.c
+++ STM32Code/Core/Src/rudder_control.c
@@ -31,6 +31,6 @@ void rudder_move_to(void) {
         }
 
         uint32_t pulse_width = 500 + (rudder_current_angle * (2000.0f / 180.0f));
-        __HAL_TIM_SET_COMPARE(rudder_htim, rudder_channel, pulse_width);
+        __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, pulse_width);
     }
 }
